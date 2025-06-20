8462f590b2d843d2de9fa4b2c761023b24517099
khh-Niloy <niloy.dev.101@gmail.com>
2025-06-21 02:10:05 +0600

    add put and delete api end points

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -15,30 +15,60 @@ const createBookZodSchema = z.object({
 });
 
 bookRouter.get("/books", async (req: Request, res: Response) => {
-  const {
-    filter,
-    sortBy = "createdAt",
-    sort = "asc",
-    limit = "10",
-  } = req.query;
+  try {
+    const {
+      filter,
+      sortBy = "createdAt",
+      sort = "asc",
+      limit = "10",
+    } = req.query;
 
-  const sortOrder = sort === "asc" ? 1 : -1;
-  const limitInt = parseInt(limit as string);
+    const sortOrder = sort === "asc" ? 1 : -1;
+    const limitInt = parseInt(limit as string);
 
-  let data;
-  if (req.query) {
-    data = await Book.aggregate([
-      { $match: { genre: filter } },
-      { $sort: { [sortBy as string]: sortOrder } },
-      { $limit: limitInt },
-    ]);
+    let data;
+    if (req.query) {
+      data = await Book.aggregate([
+        { $match: { genre: filter } },
+        { $sort: { [sortBy as string]: sortOrder } },
+        { $limit: limitInt },
+      ]);
+    }
+    data = await Book.find({});
+
+    res.json({
+      query: { filter, sortBy, sort, limit },
+      books: data,
+    });
+  } catch (error: any) {
+    const { message, name, ...otherInfo } = error;
+    res.json({
+      errorMessage: message,
+      errorName: name,
+      others: otherInfo,
+    });
   }
-  data = await Book.find({});
+});
 
-  res.json({
-    query: { filter, sortBy, sort, limit },
-    books: data,
-  });
+bookRouter.get("/books/:bookId", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.bookId;
+
+    const singleBook = await Book.findById(id);
+
+    res.json({
+      success: true,
+      message: "Book retrieved successfully",
+      data: singleBook,
+    });
+  } catch (error: any) {
+    const { message, name, ...otherInfo } = error;
+    res.json({
+      errorMessage: message,
+      errorName: name,
+      others: otherInfo,
+    });
+  }
 });
 
 bookRouter.post("/create-book", async (req: Request, res: Response) => {
@@ -54,4 +84,34 @@ bookRouter.post("/create-book", async (req: Request, res: Response) => {
   } catch (error) {
     res.send(error);
   }
+});
+
+bookRouter.put("/books/:bookId", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.bookId;
+    const updatedInfo = req.body;
+
+    const updateBookInfo = await Book.findByIdAndUpdate(id, updatedInfo, {
+      new: true,
+    });
+
+    res.json({
+      success: true,
+      message: "Book updated successfully",
+      data: updateBookInfo,
+    });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+bookRouter.delete("/books/:bookId", async (req: Request, res: Response) => {
+  const id = req.params.bookId;
+  await Book.findByIdAndDelete(id);
+
+  res.json({
+    success: true,
+    message: "Book deleted successfully",
+    data: null,
+  });
 });
