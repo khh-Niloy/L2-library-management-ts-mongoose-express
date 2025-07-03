92cab54d2ef28629702a73d8d3f44ba289fdc801
khh-Niloy <niloy.dev.101@gmail.com>
2025-07-03 22:32:18 +0600

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ import express, { Request, Response } from "express";
 import { bookRouter } from "./app/controllers/book.controller";
 import { borrowRouter } from "./app/controllers/borrow.controller";
 export const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["*"],
+    credentials: true,
+  })
+);
 
 app.use("/api/books", bookRouter);
 app.use("/api/borrow", borrowRouter);
